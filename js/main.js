188b43eda477ceb171e978b6dd7cff3ce6a54953
AmorimDSJM <AmorimDSJM@rpn.s2.ch>
2023-11-17 16:31:08 +0100

    Fixing problems in Create Agent

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict'
+function createAgent(faction, symbol){
+  const data = {faction: faction, symbol: symbol}
+  const settings = {
+   async: true,
+   crossDomain: true,
+   url: 'https://api.spacetraders.io/v2/register',
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json',
+     'Accept': 'application/json'
+   },
+   processData: false,
+   data: data
+ };
+  
+ $.ajax(settings).done(function (response) {
+  console.log(response.data);
+   return response.data;
+ });
+
+}
+
+$(document).ready(function() {
+   let agent = createAgent("COSMIC","Fewsgereb5");
+  
+})
+
+
+
