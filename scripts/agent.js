c91a3b6f72192ccc5008d83561e68365d5be55a5
Makaci Michael Gabriel <MakaciMG@s2.rpn.ch>
2023-11-24 16:11:34 +0100

    Ajout createAgent, getAgent, listAgents, getPublicAgent, getWaypoint, getMarket et correction de listSystems et de listWaypointsInSystems

---
+++ scripts/agent.js
@@ -0,0 +1,84 @@
+export async function createAgent(symbol, faction) {
+  let agent;
+
+  await $.ajax("https://api.spacetraders.io/v2/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      symbol: symbol,
+      faction: faction,
+    }),
+    success: function (response) {
+      agent = response.data;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  return agent;
+}
+
+export async function getAgent(token) {
+  let agent;
+
+  await $.ajax("https://api.spacetraders.io/v2/my/agent", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    success: function (response) {
+      agent = response.data;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  return agent;
+}
+
+export async function listAgents(limit, page) {
+  let agents;
+
+  await $.ajax("https://api.spacetraders.io/v2/agents", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+    data: {
+      limit: limit,
+      page: page,
+    },
+    success: function (response) {
+      agents = response.data;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  return agents;
+}
+
+export async function getPublicAgent(agentSymbol) {
+  let agent;
+
+  await $.ajax(`https://api.spacetraders.io/v2/agents/${agentSymbol}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+    success: function (response) {
+      agent = response.data;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  return agent;
+}
---
+++ scripts/systems.js
@@ -0,0 +1,113 @@
+export async function listSystems(limit, page) {
+  let systems;
+
+  await $.ajax("https://api.spacetraders.io/v2/systems/", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+    data: {
+      limit: limit,
+      page: page,
+    },
+    success: function (response) {
+      systems = response.data;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  return systems;
+}
+
+export async function getSystem(systemSymbol) {
+  let system;
+
+  await $.ajax(`https://api.spacetraders.io/v2/systems/${systemSymbol}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+    success: function (response) {
+      system = response.data;
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+
+  return system;
+}
+
+export async function listWaypointsInSystem(limit, page, systemSymbol) {
+  let waypoints;
+
+  await $.ajax(
+    `https://api.spacetraders.io/v2/systems/${systemSymbol}/waypoints`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+      data: {
+        limit: limit,
+        page: page,
+      },
+      success: function (response) {
+        waypoints = response.data;
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    }
+  );
+
+  return waypoints;
+}
+
+export async function getWaypoint(systemSymbol, waypointSymbol) {
+  let waypoint;
+
+  await $.ajax(
+    `https://api.spacetraders.io/v2/systems/${systemSymbol}/waypoints/${waypointSymbol}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+      success: function (response) {
+        waypoint = response.data;
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    }
+  );
+
+  return waypoint;
+}
+
+export async function getMarket(systemSymbol, waypointSymbol, token) {
+  let market;
+
+  await $.ajax(
+    `https://api.spacetraders.io/v2/systems/X1-KD70/waypoints/X1-KD70-AA1X/market`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      success: function (response) {
+        console.log(response);
+        market = response;
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    }
+  );
+
+  return market;
+}
