9de2508a214435a420157cbcfca0462dd7b14519
Marchand Arnaud <arnaud.marchand@rpn.ch>
2023-12-01 16:22:49 +0100

    Add canevas

--- js/SpaceTraders.js
+++ js/SpaceTraders.js
@@ -58,4 +58,18 @@ $(document).ready(function(){
   let systems = listSystem("6", "20")
 })
 
+let ScreenWidht = window.innerWidht;
+let ScreenHeight = window.innerHeight;
+let canvas = document.getElementById("monCanevas");
+let contexte = canvas.getContext("2d");
+var monCanevas = document.getElementById('monCanevas'); // Obtenez une référence vers votre <link>canevas</link>
+monCanevas.width = 800;
+monCanevas.height = 500;
+
+// Parcourir les rectangles du canevas
+contexte.beginPath();
+contexte.rect(100, 100, 100, 100);
+contexte.fillStyle = "black";
+contexte.fill();
+
 //type?MARKET//
