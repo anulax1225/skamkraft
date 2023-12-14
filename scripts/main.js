608a59ebf37e7ded9de66325f07d21faaa75b26f
Ruben Tome <Ruben.Tome@rpn.ch>
2023-12-14 11:46:23 +0100

    Affichage des infos des contrats

--- scripts/main.js
+++ scripts/main.js
@@ -35,9 +35,9 @@ $(document).ready(async function () {
   $("#btn-contract").on("click", () => {
     contract();
   });
-  $("#btn-faction").on("click", () => {});
-  $("#btn-ship").on("click", () => {});
-  $("#btn-system").on("click", () => {});
+  $("#btn-faction").on("click", () => { });
+  $("#btn-ship").on("click", () => { });
+  $("#btn-system").on("click", () => { });
   $("#btn-logout").on("click", () => {
     logout();
   });
@@ -60,17 +60,50 @@ async function agent() {
 }
 
 async function contract() {
+
   const token = localStorage.getItem("token");
   const contracts = await SpaceTraders.Contract.list(token);
 
-  $("main").empty();
+  $('main').empty()
 
-  contracts.forEach((contract) => {
-    let card = `<div class="card">
-      <div class="card-title">
-        <h1>${contract.id}</h1>
+  contracts.forEach(contrat => {
+    console.log(contrat);
+    const card =
+      `                            
+        <div class="card" style="width: 20rem;">
+              <img src="/img/contrat.jpg" class="card-img-top" alt="">
+              <div class="card-body">
+                  <h5 style="color:white" class="card-title">${contrat.factionSymbol}</h5>
+                  <p style="color:white" class="card-text">${contrat.accepted}</p>
+                  <button id="btn-infos" contratID="${contrat.id}" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Infos</button>
+                  <button id="btn-accepter" class="btn-modify btn btn-primary" data-toggle="modal" data-target="#Modify" >Accepter</button>                       
+              </div>
+          </div>
+      `
+
+    $('main').append(card)
+    $('#btn-infos').on('click', async function () {
+      const token = localStorage.getItem("token");
+      const contrat = await SpaceTraders.Contract.get($(this).attr('contratID'), token);
+      const modal = `
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">${contrat.id}</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
       </div>
-    </div>;`;
-    $("main").append(card);
-  });
+    </div>`
+      $('main').append(modal)
+    })
+  })
 }
