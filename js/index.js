dcec328025d069bff8677da443f1f4479ade1518
anulax1225 <anulax1225@icloud.com>
2023-12-14 18:21:15 +0100

    Factoring

---
+++ js/index.js
@@ -0,0 +1,12 @@
+import {UIRenderer} from "./ui/templeting_engine.js";
+
+let UI = new UIRenderer("html");
+
+UI.render("templates/test.html");
+
+UI.add_event("#btn1", "click", () => {
+    UI.render("templates/test2.html");
+});
+UI.add_event("#btn2", "click", () => {
+    UI.render("templates/test.html");
+})
---
+++ js/ui/templeting_engine.js
@@ -0,0 +1,27 @@
+export class UIRenderer {
+  constructor(path) {
+    this.templatePath = path;
+  }
+  render(template) {
+    this.get_template((reponse) => {
+      $('body').html(reponse);
+      this.get_template((reponse) => {
+        $('#block-content').html(reponse);
+      }, template)
+    });
+  }
+  get_template(callback, template = "") {
+    let url = template === "" ? `${this.templatePath}/template.html`: `${this.templatePath}/${template}`;
+    let data = $.ajax(url,{
+      async: false,
+      method: "GET",
+      success: callback,
+      fail: (err) => {
+        console.log(err);
+      }
+    });
+  }
+  add_event(tag, action, callback) {
+    $("body").on(action, tag, callback);
+  }
+}
