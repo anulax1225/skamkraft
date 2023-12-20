a99ffd45447bfb6a03093fa8087d9cbd1c82bd9f
anulax1225 <anulax1225@icloud.com>
2023-12-21 00:43:21 +0100

    Proto login methode with token storage and erase

--- js/index.js
+++ js/index.js
@@ -1,6 +1,47 @@
 // Copyright © 2023 Entreprise SkamKraft
 'use strict';
+import { Modal } from "./skama_code/ui/modal.js";
+import { TemplateEngine } from "./skama_code/ui/templeting_engine.js";
+import { AgentBuilder } from "./skama_code/api/agent.js";
+import { Auth } from "./skama_code/auth/auth.js";
 
-import tests from "./test/tests.js";
+let temp_engine = new TemplateEngine("html");
+let auth = new Auth(true);
+let modal = new Modal("login-modal", temp_engine);
 
-tests.modal();
+temp_engine.add_event("#ok", "click", () => {
+    temp_engine.render("templates/home.html");
+});
+
+temp_engine.add_event("#forget", "click", () => {
+    auth.unload_token();
+    modal.close();
+});
+
+temp_engine.add_event("#val", "click", () => {
+    let token = $("#in-token").val();
+    auth.login(token);
+});
+
+temp_engine.add_event("#cancel", "click", () => {
+    $("#in-token").val("");
+});
+
+temp_engine.render("templates/login.html");
+
+modal.add_class("ext-modal");
+modal.render("templates/login_modal.html");
+
+auth.done((agent) => {
+    modal.show();
+}).fail((errs) => {
+    $(".errors").html("");
+    errs.forEach(err => {
+        $(".errors").append(`
+            <p>${err}</p>
+        `);
+    });
+});
+
+auth.relog();
+
