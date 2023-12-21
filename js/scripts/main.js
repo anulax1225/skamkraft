66052f1f7fd485a920f691e4832a638081f9af93
anulax1225 <anulax1225@icloud.com>
2023-12-22 00:20:42 +0100

    Added buttons only visible if loged in and registration

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -3,13 +3,95 @@ import { Auth } from "../skama_code/auth/auth.js";
 
 let my_agent = null;
 
-function home(temp_engine) {
+function init_menu(temp_engine) {
+    temp_engine.add_event("#reg-link", "click", () => {
+        reg(temp_engine);
+    });
+    temp_engine.add_event("#login-link", "click", () => {
+        login(temp_engine);
+    });
+    temp_engine.add_event(".nav-brand", "click", () => {
+        home(temp_engine);
+    });
+}
+
+function loged_links() {
+    $(".nav-links").prepend(`
+        <li class="nav-link smooth" id="contracts-link">Contracts</li>
+        <li class="nav-link smooth" id="ships-link">Ships</li>
+    `);
+}
+
+function menu_mod(temp_engine) {
+    init_menu(temp_engine);
+    if(my_agent) {
+        $(".pseudo").text(`Agent name : ${my_agent.name}`);
+        loged_links();
+    }
+}
+
+export function home(temp_engine) {
+    temp_engine.after_render(menu_mod);
     temp_engine.render("templates/home.html");
 }
 
+export function reg(temp_engine) {
+    let active = false;
+    let auth = new Auth(true);
+    let modal = new Modal("reg-modal", temp_engine);
 
-function menu_mod() {
-    if(my_agent && my_agent.name) $(".pseudo").text(`Agent name : ${my_agent.name}`);
+    function render_reg() {
+        temp_engine.render(`templates/reg.html`);
+        modal.load("templates/reg_modal.html")
+    }
+
+    modal.add_class("ext-modal");
+    temp_engine.after_render(menu_mod);
+
+    render_reg();
+
+    temp_engine.add_event("#ok", "click", () => {
+        home(temp_engine);
+    });
+
+    temp_engine.add_event("#forget", "click", () => {
+        my_agent = null;
+        auth.unload_token();
+        modal.close();
+        render_reg();
+    });
+
+    temp_engine.add_event("#val", "click", () => {
+        if (!active) {
+            active = true;
+            let name = $("#in-name").val();
+            let faction = $("#in-faction").val();
+            auth.register({
+                name: name,
+                faction: faction
+            });
+        }
+    });
+
+    temp_engine.add_event("#cancel", "click", () => {
+        $("#in-name").val("");
+        $("#in-faction").val("");
+    });
+
+    auth.done((agent) => {
+        $(".show-token").text(agent.token);
+        modal.show();
+        my_agent = agent;
+        active = false;
+    }).fail((errs) => {
+        $(".errors").html("");
+        errs.forEach(err => {
+            $(".errors").append(`
+                <p>${err}</p>
+            `);
+        });
+        active = false;
+    });
 }
 
 export function login(temp_engine) {
