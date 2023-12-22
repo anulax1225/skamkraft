b3a3cf0808b47195195e56b81abdbf0dbf9c6074
anulax1225 <anulax1225@icloud.com>
2023-12-22 13:28:15 +0100

    Segmented functions for rendering pages in there own file

---
+++ js/scripts/home.js
@@ -0,0 +1,9 @@
+import menu_mod from "./menu_mod.js";
+
+export default function home(temp_engine) {
+    temp_engine.after_render(menu_mod);
+    temp_engine.render("templates/home.html");
+}
+
+
+
---
+++ js/scripts/login.js
@@ -0,0 +1,54 @@
+import { Modal } from "../skama_code/ui/modal.js";
+import { Auth } from "../skama_code/auth/auth.js";
+import { My } from "../skama_code/api/agent.js";
+import home from "./home.js";
+import menu_mod from "./menu_mod.js";
+
+export default function login(temp_engine) {
+    let auth = new Auth(true);
+    let modal = new Modal("login-modal", temp_engine);
+
+    function render_login() {
+        temp_engine.render(`templates/login.html`);
+        modal.load("templates/login_modal.html")
+    }
+
+    modal.add_class("ext-modal");
+    temp_engine.after_render(menu_mod);
+
+    render_login();
+
+    temp_engine.add_event("#ok", "click", () => {
+        home(temp_engine);
+    });
+
+    temp_engine.add_event("#forget", "click", () => {
+        My.agent = null;
+        auth.unload_token();
+        modal.close();
+        render_login();
+    });
+
+    temp_engine.add_event("#val", "click", () => {
+        let token = $("#in-token").val();
+        auth.login(token);
+    });
+
+    temp_engine.add_event("#cancel", "click", () => {
+        $("#in-token").val("");
+    });
+
+    auth.done((agent) => {
+        modal.show();
+        My.agent = agent;
+    }).fail((errs) => {
+        $(".errors").html("");
+        errs.forEach(err => {
+            $(".errors").append(`
+                <p>${err}</p>
+            `);
+        });
+    });
+
+    auth.relog();
+}
