d3b1b17aacf68b57d9acc3c6cc70f3d3ea9e7596
Makaci Michael Gabriel <MakaciMG@s2.rpn.ch>
2024-01-26 11:42:56 +0100

    Test API ship

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,10 +1,27 @@
 import menu_mod from "./menu_mod.js";
+import { My } from "../skama_code/api/agent.js";
+import { Ship } from "../skama_code/api/ship.js";
 
 export default function home(temp_engine) {
-    console.log("test");
-    temp_engine.after_render(menu_mod);
-    temp_engine.render("templates/home.html");
+  console.log("test");
+  temp_engine.after_render(menu_mod);
+  temp_engine.render("templates/home.html");
+
+  if (My.agent) {
+    Ship.list((ships) => {
+      ships[0].orbit(
+        (response) => console.log(response),
+        (err) => console.log(err)
+      );
+      ships[0].navigate(
+        "X1-TN14-A2",
+        (response) => {
+          console.log(response);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    });
+  }
 }
-
-
-
