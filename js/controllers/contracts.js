cb2c307696a03e5df0f6554ba39aac4daab4b09d
Ruben Tome <Ruben.Tome@rpn.ch>
2024-01-26 12:11:53 +0100

    Interface des contracts

--- js/controllers/contracts.js
+++ js/controllers/contracts.js
@@ -11,9 +11,11 @@ import { Modal } from "../skama_code/ui/modal.js";
 
 export default (temp_engine) => {
 
+
   let modal = new Modal("contract-modal", temp_engine);
 
   temp_engine.after_render((temp_engine) => {
+
     modal.load("templates/contracts/contracts_modal.html");
 
     Contract.list(10, 1, (contracts) => {
@@ -57,6 +59,8 @@ export default (temp_engine) => {
         let status
         let card
 
+
+
         if (contract.type = "PROCUREMENT") {
           img = "/assets/contracts/procurement.png"
         }
@@ -71,37 +75,45 @@ export default (temp_engine) => {
           status = "accepted"
           card =
             `                            
-                    <div class="card spacer" style="width: 20rem;">
+                    <div class="card" style="width: 20rem;">
                       <img src="${img}" class="card-img-top" alt="">
-                        <div class="card-body">
-                          <h5 style="color:white" class="card-title">${contract.faction}</h5>
-                          <p style="color:white" class="card-text">${contract.deadline}</p>
-                          <p class="card-text status-accepted">Status : ${status}</p>
-                          <button data-id="${contract.id}" class="btn-infos" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Infos</button>                      
-                        </div>
+                      <div class="card-body">
+                        <h5 style="color:white" class="card-title">${contract.faction}</h5>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p class="card-text status-accepted">Status : ${status}</p>
+                        <button data-id="${contract.id}" type="button" class="btn btn-primary btn-infos" data-bs-toggle="modal" data-bs-target="#exampleModal">Infos</button>                      
+                      </div>
                     </div>
-                  `
+            `
         }
 
         else {
           status = "on hold"
           card =
             `                            
-                    <div class="card spacer" style="width: 20rem;">
+                    <div class="card" style="width: 20rem;">
                       <img src="${img}" class="card-img-top" alt="">
-                        <div class="card-body">
-                          <h5 style="color:white" class="card-title">${contract.faction}</h5>
-                          <p style="color:white" class="card-text">${contract.deadline}</p>
-                          <p class="card-text status-onhold">Status : ${status}</p>
-                          <p style="color:white" class="card-text revenu">Revenu : ${contract.paymentAccepted} $</p>
-                          <button type="button" class="btn btn-primary btn-infos" data-bs-toggle="modal" data-bs-target="#exampleModal">Infos</button>
-                          <button data-id="${contract.id}" class="btn-modify btn btn-primary btn-accept" data-toggle="modal" data-target="#Modify" >Accepter</button>                       
-                        </div>
+                      <div class="card-body">
+                        <h5 style="color:white" class="card-title">${contract.faction}</h5>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p style="color:white" class="card-text">${contract.deadline}</p>
+                        <p class="card-text status-onhold">Status : ${status}</p>
+                        <p style="color:white" class="card-text revenu">Revenu : ${contract.paymentAccepted} $</p>
+                        <button type="button" class="btn btn-primary btn-infos" data-bs-toggle="modal" data-bs-target="#exampleModal">Infos</button>
+                        <button data-id="${contract.id}" class="btn-modify btn btn-primary btn-accept" data-toggle="modal" data-target="#Modify" >Accepter</button>                       
+                      </div>
                     </div>
                   `
         }
 
         $('.contracts').append(card);
+        console.log(contract)
 
       });
 
