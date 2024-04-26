3b275c8bd47d985c3fc5400be64746ab5c686a7a
Makaci Michael Gabriel <MichaelGabriel.Makaci@rpn.ch>
2024-04-26 10:32:33 +0200

    Disabled no recruting faction

--- js/controllers/reg.js
+++ js/controllers/reg.js
@@ -1,52 +1,58 @@
 import { Auth } from "../skama_code/auth/auth.js";
 import { My } from "../skama_code/api/agent.js";
-import { Faction } from "../skama_code/api/faction.js"
+import { Faction } from "../skama_code/api/faction.js";
 import home from "./home.js";
 import login from "./login.js";
 import menu_mod from "./menu_mod.js";
 
 export default function reg(temp_engine) {
-    let auth = new Auth();
+  let auth = new Auth();
 
-    temp_engine.after_render((temp_engine) => {
-        menu_mod(temp_engine);
+  temp_engine.after_render((temp_engine) => {
+    menu_mod(temp_engine);
 
-        Faction.list_all((factions) => {
-            factions.forEach(faction => {
-                const option = `<option>${faction.symbol}</option>`;
-                $("#group-faction").append(option);
-            });
-        });
+    Faction.list_all((factions) => {
+      factions.forEach((faction) => {
+        let option;
+        if (faction.isRecruting) {
+          option = `<option>${faction.symbol}</option>`;
+        } else {
+          option = `<option disabled>${faction.symbol}</option>`;
+        }
 
-        temp_engine.add_event("#btn-register", "click", () => {
-            const is_checked = $("#in-remember").is(":checked");
-            const symbol = $("#in-name").val();
-            const faction = $("#in-faction").val();
-            auth.store = is_checked;
-            auth.register({name: symbol, faction: faction});
-        });
-
-        temp_engine.add_event("#btn-cancel", "click", () => {
-            $("#in-name").val("");
-            $("#in-remember").prop("checked", false);
-        });
-
-        temp_engine.add_event("#btn-log", "click", () => {
-            login(temp_engine);
-        })
+        $("#group-faction").append(option);
+      });
     });
 
-    auth.done((agent) => {
-        My.agent = agent;
-        home(temp_engine);
+    temp_engine.add_event("#btn-register", "click", () => {
+      const is_checked = $("#in-remember").is(":checked");
+      const symbol = $("#in-name").val();
+      const faction = $("#in-faction").val();
+      auth.store = is_checked;
+      auth.register({ name: symbol, faction: faction });
     });
 
-    auth.fail((errors) => {
-        $(".errors").html("");
-        errors.forEach(error => {
-            $(".errors").append(`<p>${error}</p>`);
-        });
-    })
+    temp_engine.add_event("#btn-cancel", "click", () => {
+      $("#in-name").val("");
+      $("#in-remember").prop("checked", false);
+    });
 
-    temp_engine.render(`templates/auth/reg.html`);
+    temp_engine.add_event("#btn-log", "click", () => {
+      login(temp_engine);
+    });
+  });
+
+  auth.done((agent) => {
+    My.agent = agent;
+    home(temp_engine);
+  });
+
+  auth.fail((errors) => {
+    $(".errors").html("");
+    errors.forEach((error) => {
+      $(".errors").append(`<p>${error}</p>`);
+    });
+  });
+
+  temp_engine.render(`templates/auth/reg.html`);
 }
